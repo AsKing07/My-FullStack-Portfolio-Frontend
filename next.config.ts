fa16c11446c76e6c84324e4ea432a9b0b0483cf6
Charbel SONON <charbelsnn@mail.com>
2025-06-23 17:10:47 +0200

    feat: update dependencies and add new configurations
    - Updated package.json to include new dependencies and scripts for linting and type-checking.
    - Enhanced postcss.config.mjs with Tailwind CSS animations and extended theme configurations.
    - Removed unused favicon.ico and globals.css files.
    - Updated layout.tsx to import new global styles and Radix UI themes.
    - Added new API utility in src/lib/api.ts for authenticated requests.
    - Introduced utility functions in src/lib/utils.ts for class name management.
    - Created a new globals.css file with Tailwind CSS and custom styles.
    - Added components.json for component configuration and aliases.
    - Updated tsconfig.json to include base URL and path aliases for better module resolution.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // Add supported experimental options here if needed
+  },
+  images:{
+     domains: [
+      'github.com',
+      'avatars.githubusercontent.com',
+      'images.unsplash.com',
+      'via.placeholder.com',
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "",
+  }
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import "../styles/globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <html lang="fr">
+      <Theme>
+              <body
+    
       >
         {children}
       </body>
+
+      </Theme>
+
     </html>
   );
 }
