6fe2d2c6dde7189659e3092e506e148042f1bfe6
Charbel SONON <charbelsnn@mail.com>
2025-11-16 22:52:13 +0100

    fix(next.config): ajouter des options de sécurité pour les images

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,9 @@ const nextConfig = {
         port: '8000',
       },
     ],
+        dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   distDir: 'build'
 };
